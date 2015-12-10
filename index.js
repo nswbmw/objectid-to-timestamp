ab8138bb29030223df989c51edbbf01115370ddf
nswbmw <gxqzk@126.com>
2015-12-10 14:09:33 +0800

    Bump 1.2.0

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports = function objectIdToTimestamp(objectId) {
+  objectId = objectId.toString();
   return parseInt(objectId.slice(0, 8), 16) * 1000 + 
          Math.floor(parseInt(objectId.slice(-6), 16) / 16777.217); // convert 0x000000 ~ 0xffffff to 0 ~ 999
 };
